23810c32aaf4ced320d7a5426f6f9420cb44e4d8
Nizzerato <degterev57@gmail.com>
2022-07-24 19:53:40 +0300

    ah

--- backend/import_db.py
+++ backend/import_db.py
@@ -6,6 +6,7 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    json = json
     def handle(self, *args, **options):
         with open(
             'data/ingredients.json', encoding='utf-8'
