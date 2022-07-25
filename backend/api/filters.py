ece4ed54c14589bae4bbb6d49c1c4d52b8f97818
Nizzerato <degterev57@gmail.com>
2022-07-25 03:14:49 +0300

    f this

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,33 +1,40 @@
-from django_filters import rest_framework
-from recipes.models import Recipe
+from django_filters import rest_framework as django_filter
 from rest_framework import filters
+from recipes.models import Recipe
 from users.models import User
 
 
-class IngredientSearchFilter(filters.SearchFilter):
-    search_param = 'name'
+class RecipeFilters(django_filter.FilterSet):
+    """Setting up filters of the recipes model."""
 
-
-class RecipeFilter(rest_framework.FilterSet):
-    author = rest_framework.ModelChoiceFilter(queryset=User.objects.all())
-    tags = rest_framework.AllValuesMultipleFilter(field_name='tags__slug')
-    is_in_favorites = rest_framework.BooleanFilter(
-        method='get_is_in_favorites'
-    )
-    is_in_shopping_cart = rest_framework.BooleanFilter(
+    author = django_filter.ModelChoiceFilter(queryset=User.objects.all())
+    tags = django_filter.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = django_filter.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = django_filter.BooleanFilter(
         method='get_is_in_shopping_cart'
     )
 
     class Meta:
-        model = Recipe
-        fields = ('author', 'tags', 'is_in_favorites', 'is_in_shopping_cart')
+        """Parametrs of filtres of the recipes model."""
 
-    def get_is_in_favorites(self, queryset, name, value):
+        model = Recipe
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        """Process filtres of the is_favorited parameter."""
         if self.request.user.is_authenticated and value:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
+        """Process filtres of the is_in_shopping_cart parameter."""
         if self.request.user.is_authenticated and value:
-            return queryset.filter(shopping_cart__user=self.request.user)
+            return queryset.filter(carts__user=self.request.user)
         return queryset.all()
+
+
+class IngredientSearchFilter(filters.SearchFilter):
+    """Setting up filter of the search ingredient model."""
+
+    search_param = 'name'
+
